380028d57a7bb980fda6efb9563422db5d6250a0
DaviBT <davi.cesarbt@gmail.com>
2023-03-14 23:06:25 -0300

    toggle button complete

--- script/script.js
+++ script/script.js
@@ -1,13 +1,26 @@
-function toggleMode(){
-    const html = document.documentElement;
+function toggleMode() {
+  const html = document.documentElement;
 
-    if(html.classList.contains('light')){
+  //theme change
+  let theme = window.matchMedia("(prefers-color-scheme:dark)").matches
+    ? "dark"
+    : "light";
+  if (theme === "light") {
+    html.classList.add("light");
+  }
 
-    }
-}
+  /* if(html.classList.contains('light')){
+        html.classList.remove('light')
+    } else {
+        html.classList.add('light')
+    } */
+  html.classList.toggle("light"); // if there is the light class, it will remove, else, it will add
 
-
-function getCurrentTheme(){
-    let theme = window.matchMedia('(prefers-color-scheme:dark)').matches ? 'dark' : 'light';
-    console.log(theme);
+  //img change
+  if (html.classList.contains("light")) {
+    const img = document.querySelector("#profile img");
+    img.setAttribute("src", "assets/profile/profile.png"); // changing the image
+  } else {
+    img.setAttribute("src", "assets/profile/profile.png");
+  }
 }
